7145131ea8fea8ddfad1acfad57639cd88e13b7a
Tony Sweets <tsweets@fedora31.localdomain>
2019-11-26 16:42:03 -0700

    1st checkin

---
+++ rts/src/main/java/org/beer30/rts/RtsApplication.java
@@ -0,0 +1,13 @@
+package org.beer30.rts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtsApplication.class, args);
+	}
+
+}
